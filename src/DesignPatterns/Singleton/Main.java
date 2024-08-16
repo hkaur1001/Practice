b5008b8ebe244a94302c278818de2fa94b90b459
Hardeep <hardeep@LAPTOP-581BC3R7.local>
2024-08-16 11:58:53 +0530

    Singleton design pattern

---
+++ src/DesignPatterns/Singleton/Main.java
@@ -0,0 +1,9 @@
+package DesignPatterns.Singleton;
+
+public class Main {
+
+     DatabaseConnection databaseConnection1 = DatabaseConnection.getInstance();
+     DatabaseConnection databaseConnection2 = DatabaseConnection.getInstance();
+
+
+}
